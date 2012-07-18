e080c3aec96761c9b7311b4a3815812d366be8af
dxwts <dxwts@onyx-international.com>
2012-07-18 09:25:49 +0800

    add lib

--- apps/prebuilt_apps/Android.mk
+++ apps/prebuilt_apps/Android.mk
@@ -1,3 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_POST_PROCESS_COMMAND := $(shell cp -r $(LOCAL_PATH)/*.apk  $(TARGET_OUT)/app/)
+LOCAL_POST_PROCESS_COMMAND := $(shell cp -r $(LOCAL_PATH)/lib/*.so  $(TARGET_OUT)/lib/)
